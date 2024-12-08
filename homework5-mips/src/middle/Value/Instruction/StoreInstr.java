f8c13a01bf5f9338e1860e58432d4ccbc1dcba1b
July <22371500@buaa.edu.cn>
2024-12-08 22:35:00 +0800

    重构StoreInstr形式

--- homework5-mips/src/middle/Value/Instruction/StoreInstr.java
+++ homework5-mips/src/middle/Value/Instruction/StoreInstr.java
@@ -1,8 +1,5 @@
 package middle.Value.Instruction;
 
-import backend.Assembly.LiAsm;
-import backend.Assembly.MemAsm;
-import backend.MipsGenerator;
 import backend.Register;
 import backend.RegisterController;
 import middle.Type.Integer32PointerType;
@@ -13,45 +10,31 @@ import middle.Value.Value;
 
 public class StoreInstr extends Instr {
     private Type pointerType;
-    private int value;
-    private String to;
-    private boolean flag;
     
     public StoreInstr(Type type, String name, int value) {
         super(type, name);
         this.pointerType = type instanceof Integer32Type ? new Integer32PointerType() : new Integer8PointerType();
-        this.value = value;
-        this.flag = false;
+        operands.add(new Value(new Type(), String.valueOf(value)));
     }
     
     public StoreInstr(Type type, Value operand, String to) {
         super(type, to);
         this.pointerType = type instanceof Integer32Type ? new Integer32PointerType() : new Integer8PointerType();
         operands.add(operand); //把值存进从父类继承的operands里
-        this.flag = true;
-    }
-    
-    public boolean isOperandRegister(Value operand) {
-        return operand.getName().charAt(0) == '%';
     }
     
     @Override
     public String toString() {
-        if (!flag) {
-            return "store " + type + " " + value + ", " + pointerType + " " + name;
-        } else {
-            return "store " + type + " " + operands.get(0).getName() + ", " + pointerType + " " + name;
-        }
+        return "store " + type + " " + operands.get(0).getName() + ", " + pointerType + " " + name;
     }
     
     @Override
     public void generateMips() {
-        if (!flag) { //store i32 1, i32* %1
-            RegisterController.getRegisterController().loadToRegisterFromMemory(String.valueOf(value), Register.T0);
-            RegisterController.getRegisterController().storeToMemoryFromRegister(Register.T0, name);
-        } else { //store i32 operand, i32* %reg2
-            RegisterController.getRegisterController().loadToRegisterFromMemory(operands.get(0).getName(), Register.T0);
-            RegisterController.getRegisterController().storeToMemoryFromRegister(Register.T0, name);
-        }
+        /*
+         * store i32 1, i32* %1
+         * store i32 operand, i32* %reg2
+         */
+        RegisterController.getRegisterController().loadToRegisterFromMemory(operands.get(0).getName(), Register.T0);
+        RegisterController.getRegisterController().storeToMemoryFromRegister(Register.T0, name);
     }
 }
